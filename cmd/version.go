231dde686366fe1535f130e5dfba5fa45422fe7d
Siddhesh Khandagale <khandagalesid02@gmail.com>
2023-06-12 19:27:35 +0530

    version

---
+++ cmd/version.go
@@ -0,0 +1,36 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of Securelee.",
+	Long:  `Print the version number of Securelee.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		c := color.New(color.FgCyan)
+		c.Println("Securelee - v0.1.1")
+		fmt.Print("\n")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+}
